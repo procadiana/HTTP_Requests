3fe3c6248e2e9d9ec527645e434d78d4991c09b7
procadiana <diana.claudia.ilinca@gmail.com>
2019-04-08 21:01:36 +0000

    Fs requests exercices

--- HTTP_Request_2/requests.js
+++ HTTP_Request_2/requests.js
@@ -1,8 +1,3 @@
-// Create a new .js file and using request, make a 'GET' request to 'https://sytantris.github.io/http-examples/future.jpg' using chaining. The chain should:
-// Use .on('error') to handle any errors
-// Use .on('response') to log the HTTPS response code, response message (response.statusMessage)
-// and content type (response.headers['content-type'])
-// Use piping and fs.createWriteStream to save the file to your working directory ('./future.jpg')
 
 var request = require('request');
 var fs = require('fs');
